ef9dba231cc00d575ab205b961476c317d7b7d92
Doronin Stanislav <mugisbrows@gmail.com>
2021-06-30 21:30:58 +0300

    0.0.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', encoding='utf-8') as f:
 setup(
     packages = find_packages(),
     name = 'eventloop',
-    version='0.0.7',
+    version='0.0.8',
     author="Stanislav Doronin",
     author_email="mugisbrows@gmail.com",
     url='https://github.com/mugiseyebrows/event-loop',
